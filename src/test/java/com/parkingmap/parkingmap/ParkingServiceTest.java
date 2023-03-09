fa4aef77a6ff62595e6fc285ee1475e48e42cae3
brdroussi <41145526+brdroussi@users.noreply.github.com>
2023-03-09 14:53:51 +0100

    Update ParkingServiceTest.java

--- src/test/java/com/parkingmap/parkingmap/ParkingServiceTest.java
+++ src/test/java/com/parkingmap/parkingmap/ParkingServiceTest.java
@@ -1,27 +1,13 @@
 package com.parkingmap.parkingmap;
 
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.parkingmap.parkingmap.dao.ParkingRepository;
 import com.parkingmap.parkingmap.dto.ParkingDTO;
 import com.parkingmap.parkingmap.dto.PlaceParkingDTO;
-import com.parkingmap.parkingmap.entities.Parking;
-import com.parkingmap.parkingmap.entities.PlaceParking;
 import com.parkingmap.parkingmap.service.impl.ParkingServiceImpl;
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import net.minidev.json.JSONObject;
-import org.json.JSONArray;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.locationtech.jts.geom.Coordinate;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -30,17 +16,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.TransformException;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.geo.Distance;
-import org.springframework.data.geo.Metrics;
 import org.springframework.http.*;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
+
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -89,7 +68,7 @@ public class ParkingServiceTest {
         parkingDTOs.add(parkingDTO);
         when(parkingDTOList.iterator()).thenReturn(parkingDTOs.iterator());
 
-        // Executer la méthode getParkings() et vérifier les résultats
+        // Appel la méthode getParkings() et vérifier les résultats
         List<ParkingDTO> result = parkingService.getParkings(0.1560, 45.531, 2f);
         assertThat(result).isNotNull();
         assertThat(result.size()).isEqualTo(1);
